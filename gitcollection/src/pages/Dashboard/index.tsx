226032e92fdf0ba8a355da4ab8c5d9ba06566ba2
landodn <landodn@gmail.com>
2021-09-21 10:24:40 -0300

    inicial

---
+++ gitcollection/src/pages/Dashboard/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repos } from './styles';
+import logo from '../../assets/logo.svg';
+
+export const Dashboard: React.FC = () =>{
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title>Catálogo de repositórios do Github</Title>
+
+      <Form>
+        <input placeholder="username/repository_name" />
+        <button type="submit">Buscar</button>
+      </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/68967867?v=4"
+            alt="Repositorio"
+          />
+          <div>
+            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
+            <p>Repositorio do mini curso gratuito de reactjs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
+    </>
+   );
+};
---
+++ gitcollection/src/pages/Repo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Repos } from '../Dashboard/styles'; 
+
+export const Repo: React.FC = () =>{
+  return   <Repos><p>repositorios</p></Repos>  
+};
+
